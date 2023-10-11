671224449e036121201556a8cdccb6c81b9a9dad
Azad Ali <71816132+azad-ali786@users.noreply.github.com>
2023-10-11 21:30:03 +0530

    Adding new languages and test

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -2,9 +2,32 @@ package main
 
 const hello = "Hello, "
 
-func Hello(name string) string {
+
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return hello + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
