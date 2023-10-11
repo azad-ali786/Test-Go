54e8d6ab2895a6e755012eb4a8dc98ec76bef551
Azad Ali <71816132+azad-ali786@users.noreply.github.com>
2023-10-11 20:49:35 +0530

    Taking user input

--- helloworld/helloworld_test.go
+++ helloworld/helloworld_test.go
@@ -2,11 +2,20 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	// func TestHello(t *testing.T) {
+	// 	got := Hello()
+	// 	want := "Hello, world"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	// 	if got != want {
+	// 		t.Errorf("got %q want %q", got, want)
+	// 	}
+	// }
+
+func TestHello(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+	
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 }
