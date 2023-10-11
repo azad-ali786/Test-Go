21ad656cf29acaefd5a176d1df0c5d811c60e781
Azad Ali <71816132+azad-ali786@users.noreply.github.com>
2023-10-11 20:59:48 +0530

    Running test with specified outputs

--- helloworld/helloworld_test.go
+++ helloworld/helloworld_test.go
@@ -12,17 +12,23 @@ import "testing"
 	// }
 
 func TestHello(t *testing.T) {
+
+	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 	
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	})
+	
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 
-		got1 := Hello("")
-		want1 := "Hello, World"
+		got := Hello("")
+		want := "Hello, World"
 	
 		if got != want {
-			t.Errorf("got %q want %q", got1, want1)
+			t.Errorf("got %q want %q", got, want)
 		}
+	})
 }
